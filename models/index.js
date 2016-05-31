915283e165dac3278878d1854c183da14a10732d
Ignacio Barrios <i.barriosp@outlook.es>
2016-05-31 16:13:12 +0200

    Tema 10: Lista de Preguntas Corregido

--- models/index.js
+++ models/index.js
@@ -35,21 +35,23 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 sequelize.sync()
     .then(function() {
         // Ya se han creado las tablas necesarias.
-        return Quiz.count()
-                .then(function (c) {
-                    if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.create({ question: 'Capital de Italia',
-                                            answer: 'Roma'
-                                          })
-                                   .then(function() {
-                                        console.log('Base de datos inicializada con datos');
-                                    });
-                    }
-                });
-    })
-    .catch(function(error) {
-        console.log("Error Sincronizando las tablas de la BBDD:", error);
-        process.exit(1);
-    });
-
-exports.Quiz = Quiz; // exportar definición de tabla Quiz
+        return Quiz.
+                count()
+                .then(function (c){
+                  if (c == 0) {
+                      return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                               {question: 'Capital de Portugal', answer: 'Lisboa'}
+                                              ])
+                    .then(function() {
+                                         console.log('Base de datos inicializada con datos');
+                                     });
+                     }
+                 });
+     })
+     .catch(function(error) {
+         console.log("Error Sincronizando las tablas de la BBDD:", error);
+         process.exit(1);
+     });
+ 
+ 
+ exports.Quiz = Quiz; // exportar definición de tabla Quiz
