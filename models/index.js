bf5b581043b35520350f7648af720b4f780b19be
Ignacio Barrios <i.barriosp@outlook.es>
2016-05-31 11:50:38 +0200

    Despliegue DB en Heroku

--- models/index.js
+++ models/index.js
@@ -4,10 +4,28 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null, 
-                       		  { dialect: "sqlite", 
-                       		    storage: "quiz.sqlite"
-                       		  });
+
+//DATABASE_URL = sqlite:///
+//DATABASE_STORAGE = quiz.sqlite
+
+//Usar BBDD Postgres:
+//  DATABASE_URL = postgres://krkwzvbvdyowkj:HnTv3J1Omp9J0OMudIayZFCQUL@ec2-54-163-239-28.compute-1.amazonaws.com:5432/dbfmehc6m1aeei
+
+var url, storage;
+
+if(!process.env.DATABASE_URL){
+  url = "sqlite:///";
+  storage = "quiz.sqlite";
+} else{
+  url = process.env.DATABASE_URL;
+  storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, 
+                             { storage: "storage", 
+                               omitNull: true
+                             });
+
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
@@ -21,8 +39,8 @@ sequelize.sync()
                 .then(function (c) {
                     if (c === 0) {   // la tabla se inicializa solo si está vacía
                         return Quiz.create({ question: 'Capital de Italia',
-          	                                 answer: 'Roma'
-          	                               })
+                                            answer: 'Roma'
+                                          })
                                    .then(function() {
                                         console.log('Base de datos inicializada con datos');
                                     });
@@ -33,6 +51,5 @@ sequelize.sync()
         console.log("Error Sincronizando las tablas de la BBDD:", error);
         process.exit(1);
     });
-
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
