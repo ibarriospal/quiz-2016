c6532fe5092f02b7124ff07ec8c14732f090939a
Ignacio Barrios <i.barriosp@outlook.es>
2016-05-31 12:56:26 +0200

    Tema 10: Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var router = express.Router();
+ var express = require('express');
+ var router = express.Router();
+ 
+ var quizController = require('../controllers/quiz_controller');
+ 
+ /* GET home page. */
+ router.get('/', function(req, res, next) {
+   res.render('index');
+ });
+ 
 
-var quizController = require('../controllers/quiz_controller');
+// Definición de rutas de /quizzes
+router.get('/quizzes',                     quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
-router.get('/question', quizController.question);
-router.get('/check', quizController.check);
-
-module.exports = router;
+ 
+ module.exports = router;
