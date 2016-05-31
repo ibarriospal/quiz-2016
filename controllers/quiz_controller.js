3453f336a5cda6f609b5f561e9cc0b19b08db0b4
Ignacio Barrios <i.barriosp@outlook.es>
2016-05-31 16:55:35 +0200

    Tema 12: Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,48 +1,73 @@
  var models = require('../models');
  
-// Autoload el quiz asociado a :quizId
-exports.load = function(req, res, next, quizId) {
- models.Quiz.findById(quizId)
-     .then(function(quiz) {
-         if (quiz) {
-           req.quiz = quiz;
-           next();
-         } else { 
-           next(new Error('No existe quizId=' + quizId));
-         }
-        })
-        .catch(function(error) { next(error); });
+ // Autoload el quiz asociado a :quizId
+ exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId)
+      .then(function(quiz) {
+          if (quiz) {
+            req.quiz = quiz;
+            next();
+          } else { 
+            next(new Error('No existe quizId=' + quizId));
+          }
+         })
+         .catch(function(error) { next(error); });
+ };
+ 
+ 
+ // GET /quizzes
+ exports.index = function(req, res, next) {
+  models.Quiz.findAll()
+    .then(function(quizzes) {
+      res.render('quizzes/index.ejs', { quizzes: quizzes});
+    })
+    .catch(function(error) {
+      next(error);
+    });
+ };
+ 
+ 
+ // GET /quizzes/:id
+ exports.show = function(req, res, next) {
+ 
+  var answer = req.query.answer || '';
+ 
+  res.render('quizzes/show', {quiz: req.quiz,
+                answer: answer});
+ };
+ 
+ 
+ // GET /quizzes/:id/check
+ exports.check = function(req, res, next) {
+ 
+  var answer = req.query.answer || "";
+ 
+  var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
+ 
+  res.render('quizzes/result', { quiz: req.quiz, 
+                   result: result, 
+                   answer: answer });
+ };
+ 
+
+ // GET /quizzes/new
+exports.new = function(req, res, next) {
+  var quiz = models.Quiz.build({question: "", answer: ""});
+  res.render('quizzes/new', {quiz: quiz});
 };
 
+// POST /quizzes/create
+exports.create = function(req, res, next) {
+  var quiz = models.Quiz.build({ question: req.body.quiz.question, 
+                                 answer:   req.body.quiz.answer} );
 
-
-// GET /quizzes
-exports.index = function(req, res, next) {
- models.Quiz.findAll()
-   .then(function(quizzes) {
-     res.render('quizzes/index.ejs', { quizzes: quizzes});
-   })
-   .catch(function(error) {
-     next(error);
-   });
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["question", "answer"]})
+   .then(function(quiz) {
+     res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
+    })
+    .catch(function(error) {
+   next(error);
+ });  
 };
 
-
-// GET /quizzes/:id
-exports.show = function(req, res, next) {
-
-       res.render('quizzes/show', {quiz: quiz,
-                     answer: answer});
-};   
-var models = require('../models');
-
-
-// GET /quizzes/:id/check
-exports.check = function(req, res) {
-	
-					var answer = req.query.answer || "";
-					var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-						res.render('quizzes/result', { quiz: quiz, 
-											   result: result, 	
-											   answer: answer });
- }; 
